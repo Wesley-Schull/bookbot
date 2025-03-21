b527b7f242b538b3efd9d3d0325771a6182bf0a8
Wes_Schull <varnathin@gmail.com>
2025-03-20 20:33:48 -0400

    main.py now outputs the word count of ./books/frankenstein.txt (75767 words)

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 # BookBot, the first of the Boot.Dev projects!
 
-def get_book_text(filepath):
+def get_book_text(file_path):
     file_text = "No file found."
     
     try:
-        with open(filepath) as f:
+        with open(file_path) as f:
             file_text = f.read()
     except Exception as e:
         print(f"Error encountered: {e}")
 
     return file_text
 
+
+def count_words(input_text):
+    words = input_text.split()
+    
+    return len(words)
+
+
 def main():
-    print(get_book_text("./books/frankenstein.txt"))
-    # print(get_book_text("f"))
+    frankenstein_text = get_book_text("./books/frankenstein.txt")
+    
+    print(f"{count_words(frankenstein_text)} words found in the document")
 
 main()
